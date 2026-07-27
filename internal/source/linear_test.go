029474455933172426cff5a6b0c1e3f5d13fc357
Fabien O'Carroll <fabien@allou.is>
2026-07-27 09:11:07 +0000

    feat(source): Get(item_ref) resolves one Item

--- internal/source/linear_test.go
+++ internal/source/linear_test.go
@@ -66,6 +66,35 @@ func TestLinearListParsesIssues(t *testing.T) {
 	}
 }
 
+const linearIssueJSON = `{"data":{"issue":{"id":"abc-1","identifier":"ENG-42","title":"Fix login","url":"https://linear.app/acme/issue/ENG-42"}}}`
+
+func TestLinearGet(t *testing.T) {
+	fd := &fakeDoer{body: linearIssueJSON}
+	src := &Linear{apiKey: "lin_xyz", doer: fd}
+
+	ref := engine.ItemRef{Source: "linear", Type: "issue", ExternalID: "abc-1"}
+	item, err := src.Get(context.Background(), ref)
+	if err != nil {
+		t.Fatalf("Get: %v", err)
+	}
+	want := Item{
+		Ref:   ref,
+		Title: "Fix login",
+		URL:   "https://linear.app/acme/issue/ENG-42",
+		Vars: map[string]string{
+			"url":        "https://linear.app/acme/issue/ENG-42",
+			"title":      "Fix login",
+			"identifier": "ENG-42",
+		},
+	}
+	if !reflect.DeepEqual(item, want) {
+		t.Errorf("item =\n %+v\nwant\n %+v", item, want)
+	}
+	if !strings.Contains(fd.sent, "abc-1") {
+		t.Errorf("request body missing issue id: %q", fd.sent)
+	}
+}
+
 func TestLinearListAuthHeader(t *testing.T) {
 	fd := &fakeDoer{body: `{"data":{"viewer":{"assignedIssues":{"nodes":[]}}}}`}
 	src := &Linear{apiKey: "lin_xyz", doer: fd}
